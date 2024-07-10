d423f71b15076117b495d34f22c0705bbbf0e66e
Flying <1724593655@qq.com>
2024-07-10 23:50:02 +0800

    更新了前后端环境配置

--- my-project-frontend/src/main.js
+++ my-project-frontend/src/main.js
@@ -5,7 +5,7 @@ import axios from "axios";
 import {createPinia} from "pinia";
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import '@/assets/quill.css'
-axios.defaults.baseURL = 'http://localhost:8080'
+axios.defaults.baseURL = 'http://localhost:80'
 const app = createApp(App)
 
 app.use(createPinia())
