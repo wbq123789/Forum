3765d864157092c97bf19d51d6052359a45362e4
Flying <1724593655@qq.com>
2024-07-11 22:12:12 +0800

    测试服务端配置_11

--- my-project-frontend/src/main.js
+++ my-project-frontend/src/main.js
@@ -5,7 +5,7 @@ import axios from "axios";
 import {createPinia} from "pinia";
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import '@/assets/quill.css'
-axios.defaults.baseURL = 'http://172.18.0.1:8888'
+axios.defaults.baseURL = 'http://47.92.80.128/:8888'
 const app = createApp(App)
 
 app.use(createPinia())
